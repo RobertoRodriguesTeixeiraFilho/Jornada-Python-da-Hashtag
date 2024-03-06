83ab434ce808d9638dda14c4756d69b431f47dd8
Roberto Rodrigues Teixeira Filho <rrteixeirafilho@gmail.com>
2024-03-06 17:38:26 -0300

    refactor: organiza imports e adiciona comentários

--- codigo.py
+++ codigo.py
@@ -12,19 +12,23 @@ Passo 4: Cadastrar 1 produto
 
 Passo 5: Repetir o processo de cadastro até acabar a base de dados
 """
-import pyautogui
 import time
+import pyautogui
 
-pyautogui.PAUSE = 1 
+pyautogui.PAUSE = 1
 
+# Passo 1: 1/2
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
+# Passo 1: 2/2
 LINK = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(LINK)
 pyautogui.press("enter")
 
 # Pausa para carregamento do site
 time.sleep(3)
+
+# Passo 2
 pyautogui.click(x=-1240, y=379)
